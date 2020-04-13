f6ab7687d1e07eed4dbc27f662e4289b5c930361
Shubha Singh <shsingh@intralinks.com>
2020-04-12 21:57:38 -0400

    Mongoose Error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps= await Bootcamp.find();
         res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
     } catch(err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
 
@@ -26,7 +26,7 @@ exports.getBootcamp = async (req, res, next) => {
         }
         res.status(200).json({success: true, data: bootcamp});
     } catch(err) {
-        next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404));
+        next(err);
     }
 };
 
@@ -38,7 +38,7 @@ exports.createBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.create(req.body);
         res.status(200).json({success: true, data: bootcamp});
     } catch(err) {
-        res.status(400).json({ success: false, msg: err });
+        next(err);
     }
 };
 
@@ -53,11 +53,11 @@ exports.updateBootcamp = async (req, res, next) => {
              runValidators: true
             });
         if(!bootcamp) {
-            res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404));
         }
         res.status(201).json({success: true, data: bootcamp });
     } catch(err) {
-        res.status(400).json({ success: false, msg: err });
+        next(err);
     }
 };
 
@@ -68,10 +68,10 @@ exports.deleteBootcamp = async (req, res, next) => {
     try {
         const bootcamp= await Bootcamp.findByIdAndDelete(req.params.id);
         if(!bootcamp) {
-            res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404));
         }
         res.status(204).json({success: true, data: {} });
     } catch(err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
