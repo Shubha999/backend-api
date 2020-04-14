2bb345e0f87ada11003e1671e880bd00eeb736db
Shubha Singh <shsingh@intralinks.com>
2020-04-14 09:32:41 -0400

    Get bootcamps within a radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,6 +2,7 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 
 // Create different methods that will be associated with specific routes
 // @desc      Get all bootcamps
@@ -55,4 +56,34 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404));
     }
     res.status(204).json({success: true, data: {} });
+});
+
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Public
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    // Calculate radius using randians(for earth sphere)
+    // Divide dist by radius of Earth
+    // Earth radius = 3963 miles /6378 km
+
+    const radius = distance/3963;
+    const bootcamps = await Bootcamp.find({
+       location: {
+           $geoWithin: {
+               $centerSphere: [ [lng, lat ], radius ]
+           }
+       }
+    });
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 // Get all conroller methods for the router
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps');
 
 //Initialize express router
 const router = express.Router();
@@ -9,5 +9,8 @@ const router = express.Router();
 // Routes for bootcamps
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+
+// Route for bootcamps by distance
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 module.exports = router;
