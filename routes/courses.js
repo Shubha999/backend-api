5d5e318ecd475d7a68100d83b117c55190f5e23c
Shubha Singh <shsingh@intralinks.com>
2020-04-16 23:30:48 -0400

    CRUD for courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 // Get all controller methods for the router
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 
 //Initialize express router
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
