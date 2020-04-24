af9dd7fc23b1cb7218fa3c0a74f25c6134a3fabc
Shubha Singh <shsingh@intralinks.com>
2020-04-24 17:08:27 -0400

    CRUD and average rating for review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 // Get all controller methods for the router
-const { getReviews, getReview } = require('../controllers/review');
+const { getReviews, getReview, addReview, updateReview, deleteReview } = require('../controllers/review');
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 
@@ -12,8 +12,8 @@ const { protect, authorize } = require('../middleware/auth');
 router.route('/').get(advancedResults(Review, {
     path: 'bootcamp',
     select: 'name description'
-}), getReviews);
+}), getReviews).post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview);
+router.route('/:id').get(getReview).put(protect, authorize('user', 'admin'),updateReview).delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
