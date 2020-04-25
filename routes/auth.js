01ede6b71652178e37d96ced6c8f7c9870b5e0fb
Shubha Singh <shsingh@intralinks.com>
2020-04-25 16:51:14 -0400

    API security- logout to clear token cookie and prevent sql injection

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword, logout } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -12,5 +12,6 @@ router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
+router.get('/logout', protect, logout);
 
 module.exports = router;
