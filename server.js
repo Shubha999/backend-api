0b590efb0533bc0da8cfc14f0ea99b8b4228a5d3
Shubha Singh <shsingh@intralinks.com>
2020-04-25 17:33:15 -0400

    APT Security- XSS Protection, security headers, rate limiting, HPP, CORS

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require("path");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const colors = require("colors");
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
@@ -41,6 +46,25 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10*60* 1000, // 10 minutes
+    max: 100 // Max request
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
